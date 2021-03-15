4de5cdda07c5d6842ba10405491f95aa07a0e2fc
Your Name <happyeyal@gmail.com>
2021-03-15 18:42:28 +0200

    code shrinked

--- Board.cpp
+++ Board.cpp
@@ -7,7 +7,6 @@
  * Date: 2021-03
  ***********************************************************************************/
 
-
 #include <iostream>
 #include "Board.hpp"
 using namespace std;
@@ -21,61 +20,43 @@ namespace ariel {
         for (int i = 0; i < r ; i++){
             this->board[i].resize(c, '_');
         }
-        cout << "Successfully resizized to " << r << " x " << c << endl;
     }
 
     void Board::post(int row, int column, Direction direction, string message) {
-        cout << "---------------------------------------------\n"; 
-        cout << "\t\tposting: \"" << message << "\" in " << row <<"x" << column << "\n";
         int msg_len = message.size();
-        if(direction == Direction::Horizontal){
-            int r = std::max(this->rows, row+1);
-            int c = std::max(this->cols, column + msg_len +1);
-            resizeBoard(r, c);
-            /* posting: */
-            for(int i=0; i<msg_len; i++){
-                this->board.at(row).at(column+i) = message[i];
-            }
+        bool flag = (direction == Direction::Horizontal);
+        if(flag){
+            resizeBoard(std::max(this->rows, row+1),
+                        std::max(this->cols, column + msg_len +1));
         }
-        
-        if(direction == Direction::Vertical){
-            int r = std::max(this->rows, row + msg_len + 1);
-            int c = std::max(this->cols, column + 1);
-            resizeBoard(r, c);
-            /* posting: */
-            for(int i=0; i<msg_len; i++){
-                this->board[row+i][column] = message[i];
-                cout << this->board[row+i][column] << "\n";
-            }    
+        else {
+            resizeBoard(std::max(this->rows, row + msg_len + 1), 
+                        std::max(this->cols, column + 1));
         }
-        cout << "Successfully posted: " << message << " in: " << row << " x " << column << endl;
-        show();
+        /* posting: */
+        for(int i=0; i<msg_len; i++){
+            this->board[row][column] = message[i];
+            flag? column++ : row++;
+        }    
     }
 //--------------------------------------------------------------------------------------------------
     string Board::read(int row, int column, Direction direction, int length) {
         string ans;
-        if(direction == Direction::Horizontal){
-            if(column + length > this->cols || row > this->rows)
-                throw out_of_range("out of the mesagges board!");
-
+        bool flag = (direction == Direction::Horizontal);
+        try {
             for(int i=0; i<length; i++){
-                ans += this->board[row][column+i];
+                ans += this->board.at(row).at(column);
+                flag? column++ : row++;
             }
         }
-        else if(direction == Direction::Vertical || column > this->cols){
-            if(row + length > this->rows)
-                throw out_of_range("out of the mesagges board!");
-
-            for(int i=0; i<length; i++){
-                ans += this->board[row+i][column];
-            }
+        catch(const std::exception& e) {
+            cout << "out of the the board!" << endl;
         }
         return ans;
     }
 //--------------------------------------------------------------------------------------------------
     void Board::show() {
-        cout << "The board: \n";
-        cout << "rows: " << this->rows << " cols: " << this->cols << "\n\n"; 
+        cout << "The board: \nrows: " << this->rows << " cols: " << this->cols << "\n\n"; 
         for (int i = 0; i < this->rows; i++) {
             for (int j = 0; j < this->cols; j++) {
                 cout << this->board[i][j] << " ";
--- Demo.cpp
+++ Demo.cpp
@@ -19,17 +19,13 @@ int main() {
 	board.post(/*row=*/0, /*column=*/7, Direction::Vertical, "Finneshtein");
 	board.post(/*row=*/2, /*column=*/9, Direction::Horizontal, "is");
 	board.post(/*row=*/3, /*column=*/12, Direction::Vertical, "the");
-	// board.post(/*row=*/5, /*column=*/1, Direction::Horizontal, "jehonathan");
-	// cout << board.read(/*row=*/1, /*column=*/20, Direction::Vertical, /*length=*/3) << endl;  
-	// 	// prints "_b_" (starts at row 99 which is empty; then at row 100 there is "b"; then row 101 is empty again).
 	board.post(/*row=*/5, /*column=*/15, Direction::Horizontal, "gayest");
-	// cout << board.read(/*row=*/100, /*column=*/200, Direction::Horizontal, /*length=*/6) << endl;  
-	// 	// prints "abyd__" (First letters are ab; then y from the "xyz"; then d; then two empty cells).
 	board.post(/*row=*/10, /*column=*/11, Direction::Horizontal, "in");
 	board.post(/*row=*/10, /*column=*/16, Direction::Vertical, "the");
 	board.post(/*row=*/10, /*column=*/19, Direction::Vertical, "world!");
-	// board.post(/*row=*/4, /*column=*/13, Direction::Horizontal, "gayest");
 
-	// board.show();
+	board.show();
+
+	cout << board.read(/*row=*/5, /*column=*/15, Direction::Horizontal, /*length=*/6) << endl;  
 }
 
